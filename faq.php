9fa5ab95322fd3a625e6c6905b94eda4f106e9cf
Kevin Bi <keveloper@gmail.com>
2018-01-26 09:43:41 -0500

    Added to FAQ

--- faq.php
+++ faq.php
@@ -11,7 +11,7 @@
     <?php echo HEADER ?>
     <h3>Where is the data from?</h3>
     <p>
-        The data is retrieved from the <a href = "http://q.fas.harvard.edu">Harvard Q-Guide</a>.
+        The data is retrieved from the <a href = "http://q.fas.harvard.edu" target="_blank">Harvard Q-Guide</a>.
     </p>
     <h3>Why are the hours weird after Fall 2014?</h3>
     <p>
@@ -27,5 +27,19 @@
         <br/>
         We take the square root of the enrollment to take class size into account but reduce the effect of extremely large class sizes.
     </p>
+
+    <h3>I think this website is broken/this website needs more features! What should I do?</h3>
+    <p>
+        The code for this website is <a href = "https://github.com/TheKeveloper/QAnalytics-PHP" target="_blank">on Github</a>. Feel free to open an issue or fork the repository and create a pull request.
+    </p>
+
+    <h3>This website is terrible! What if I just want the data?</h3>
+    <p>
+        If you have a @college.harvard.edu email address, you can access the raw dataset in <a href = "https://docs.google.com/spreadsheets/d/1XPSSKI3-HSqMGgO_OXUNdNqcAmlym3seVUSk7G2-rb4/edit?usp=sharing" target="_blank"> a Google Spreadsheet. </a>
+    </p>
+
+    <div id = "footer" align = "center">
+        <?php echo FOOTER ?>
+    </div>
 
 </html> 
--- server/all.php
+++ server/all.php
@@ -3,8 +3,8 @@
     include_once("Course.php");
     include_once("Department.php");
     define("TITLE", "Q-Analytic");
-    define("FOOTER", "Data from q.fas.harvard.edu <br/>
-    <a href = \"https://docs.google.com/spreadsheets/d/1XPSSKI3-HSqMGgO_OXUNdNqcAmlym3seVUSk7G2-rb4/edit?usp=sharing\">Raw dataset</a><br/>
+    define("FOOTER", "Data from <a href = \"http://q.fas.harvard.edu\" target=\"_blank\">q.fas.harvard.edu </a><br/>
+    <a href = \"https://docs.google.com/spreadsheets/d/1XPSSKI3-HSqMGgO_OXUNdNqcAmlym3seVUSk7G2-rb4/edit?usp=sharing\" target=\"_blank\">Raw dataset</a><br/>
     Created by <a href = \"mailto:kevinbi@college.harvard.edu\">Kevin Bi</a>.");
     define("HEADER",
     "<a href = \"index.php\" id = \"pageTitle\">" . TITLE . "</a>
