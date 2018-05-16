96ac129a90101cd386769f2385cd35f725497195
Kevin Bi <keveloper@gmail.com>
2018-05-16 11:04:51 -0500

    Fixed bug with link to my harvard

--- courses.php
+++ courses.php
@@ -29,7 +29,7 @@
 			?>
 
 			<a href = "<?php
-				echo str_replace("DEPT%20NUM", str_replace($course->code, " ", "%20"),"https://courses.my.harvard.edu/psp/courses/EMPLOYEE/EMPL/h/?tab=HU_CLASS_SEARCH&SearchReqJSON=%7B%22PageNumber%22%3A1%2C%22PageSize%22%3A%22%22%2C%22SortOrder%22%3A%5B%22IS_SCL_SUBJ_CAT%22%5D%2C%22Facets%22%3A%5B%5D%2C%22Category%22%3A%22HU_SCL_SCHEDULED_BRACKETED_COURSES%22%2C%22SearchPropertiesInResults%22%3Atrue%2C%22FacetsInResults%22%3Atrue%2C%22SaveRecent%22%3Atrue%2C%22TopN%22%3A%22%22%2C%22SearchText%22%3A%22DEPT%20NUM%22%7D");
+				echo (str_replace("SEARCHQUERY", str_replace(" ", "", $course->code),"https://courses.my.harvard.edu/psp/courses/EMPLOYEE/EMPL/h/?tab=HU_CLASS_SEARCH&SearchReqJSON=%7B%22PageNumber%22%3A1%2C%22PageSize%22%3A%22%22%2C%22SortOrder%22%3A%5B%22IS_SCL_SUBJ_CAT%22%5D%2C%22Facets%22%3A%5B%5D%2C%22Category%22%3A%22HU_SCL_SCHEDULED_BRACKETED_COURSES%22%2C%22SearchPropertiesInResults%22%3Atrue%2C%22FacetsInResults%22%3Atrue%2C%22SaveRecent%22%3Atrue%2C%22TopN%22%3A%22%22%2C%22SearchText%22%3A%22SEARCHQUERY%22%7D"));
 			?>" target="_blank">Search on my.harvard</a>
 		</div>
 	</div>
